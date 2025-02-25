b64a5a8dee4e4ab3984c3f17915ae4f4ba4228ec
KHE <papernote39@gmail.com>
2025-02-25 14:44:14 +0900

    테스트 데모 업로드

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "FourDArt-Wonju_AR-0.1.0";
+const contentToCache = [
+    "Build/Wonju_AR_Release.loader.js",
+    "Build/Wonju_AR_Release.framework.js.unityweb",
+    "Build/Wonju_AR_Release.data.unityweb",
+    "Build/Wonju_AR_Release.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
