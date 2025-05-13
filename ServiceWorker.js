6468af778d406c89afa9eaa14584515c9332996c
SignKHE <papernote39@gmail.com>
2025-05-13 20:57:44 +0900

    버전업

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "FourDArt-Wonju_AR-0.1.19";
+const cacheName = "FourDArt-Wonju_AR-0.1.20";
 const contentToCache = [
     "Build/Wonju_AR_Web.loader.js",
     "Build/Wonju_AR_Web.framework.js.unityweb",
